7dcbd8e1f2d09e7644b331711fa69866fde55eaa
surjeetvermaiitm <surjeetverma2017@gmail.com>
2022-10-19 19:51:21 +0530

    react module 3

--- module-3-react-basic/react-complete-guide/src/components/NewExpense/ExpenseForm.js
+++ module-3-react-basic/react-complete-guide/src/components/NewExpense/ExpenseForm.js
@@ -1,12 +1,15 @@
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+  const titleChangeHandler = () => {
+    console.log("Title Changed");
+  };
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text"></input>
+          <input type="text" onChange={titleChangeHandler}></input>
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
